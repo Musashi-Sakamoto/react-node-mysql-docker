756a206c760964c08b69a14f7e774cf405e76683
MusashiSakamoto <1292602b@gmail.com>
2018-09-29 08:52:27 +0900

    sequelize 導入

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var models = require('../models')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  models.employee.all().then(employees => {
+    res.render('index', {
+      title: employees.length
+    });
+  })
 });
 
 module.exports = router;
