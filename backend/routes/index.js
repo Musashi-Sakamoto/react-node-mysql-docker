87f36ca663125c243948e6d56a43ca0ecbcc5f82
MusashiSakamoto <1292602b@gmail.com>
2018-09-29 09:35:37 +0900

    data persisted

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var models = require('../models')
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  models.employee.all().then(employees => {
-    res.render('index', {
-      title: employees.length
-    });
+  res.render('index', {
+    title: 'Express'
   })
 });
 
