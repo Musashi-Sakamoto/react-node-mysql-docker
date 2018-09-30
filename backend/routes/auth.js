c00d116ff2ec3d7f90f34877222c37a1047dd382
MusashiSakamoto <1292602b@gmail.com>
2018-09-30 19:16:13 +0900

    logout and expiration token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,13 +20,25 @@ router.post('/login', (req, res, next) => {
             if (err) {
                 res.send(err)
             }
-            const token = jwt.sign(user, 'your_jwt_secret')
+            const token = jwt.sign(user, 'your_jwt_secret', {
+                expiresIn: '30 days'
+            })
             return res.json({
                 user,
                 token
             })
         })
     })(req, res)
+})
+
+router.get('/logout', passport.authenticate('jwt', {
+    session: false
+}), (req, res, next) => {
+    req.logout()
+    res.status(200).json({
+        user: null,
+        token: null
+    })
 })
 
 module.exports = router
